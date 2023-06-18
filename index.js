bdad4a86274b7701277513da6db8ff3c82f9c423
Marie-Geneste <marie.e.geneste@gmail.com>
2023-06-18 12:03:53 +0200

    deploy railway

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
 app.use(router);
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
     console.log(`Server listing at : http://localhost:${PORT}`);
 });
